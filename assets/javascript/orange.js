b12cc759425c426665dfc7f1e1ec434874105bd7
KMZ <mzlatnik@gmail.com>
2019-03-09 11:26:45 -0800

    working wikipedia and doubleclick functions

--- assets/javascript/orange.js
+++ assets/javascript/orange.js
@@ -13,16 +13,30 @@ function allSearch(word) {
 function wikiSearch() {    
     var queryURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&uselang=user&errorformat=plaintext&search=' + searchTerm;   
     callWikipedia(queryURL).then(function(response) {
-    
     //use this when switching to actual ajax and delete the line above
     /* $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
     */
-    const resultPara = console.log(JSON.parse(response));    
+    const resultPara = JSON.parse(response);    
     $("#wikiTitle").text(resultPara[0]);
-    var wikiPara = $("<p>").append(resultPara[2]);
+    var wikiPara = $("<p id='wikiParagraph'>")
+        .append(resultPara[2])
+        .css({cursor: "pointer"})
+        .dblclick(function(e) {
+            alert("you clicked me!")
+          var range =
+            window.getSelection() ||
+            document.getSelection() ||
+            document.selection.createRange();
+          searchTerm = $.trim(range.toString());
+          console.log(searchTerm)
+          if (searchTerm != "") {
+            allSearch(searchTerm)
+          }
+          e.stopPropagation();
+        });
     $("#wikiPara").append(wikiPara);
     });    
 };
@@ -37,21 +51,9 @@ function wikiSearch() {
 } */
 
 //double click function for grabbing search terms
-var p = $("p");
-p.css({ cursor: "pointer" });
-p.dblclick(function(e) {
-  var range =
-    window.getSelection() ||
-    document.getSelection() ||
-    document.selection.createRange();
-  searchTerm = $.trim(range.toString());
-  if (searchTerm != "") {
-    alert(searchTerm);
-      allSearch(searchTerm)
-  }
-  range.collapse();
-  e.stopPropagation();
-});
+/* $(document).on("click", ".transit", displayGos); */
+
+
 
 //click function for search button
 $("#basic-text").on("click", function(event) {
