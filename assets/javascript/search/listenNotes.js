2d42ec49d90e1ce9574a1575ecf09738b52ff297
KMZ <mzlatnik@gmail.com>
2019-03-14 19:38:57 -0700

    a couple small changes

--- assets/javascript/search/listenNotes.js
+++ assets/javascript/search/listenNotes.js
@@ -1,4 +1,4 @@
-function youTubeSearch(searchTerm) {
+function listenNotesSearch() {
 	const key = prompt('give me ur youtube key');
 
 	const queryURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q='+ searchTerm + '&key=' + key;
@@ -9,22 +9,22 @@ function youTubeSearch(searchTerm) {
 		}).then(response => {
 			const randChoice = response.items[Math.floor(Math.random()*response.items.length)];
 			const videoId = randChoice.id.videoId;
-	    const url = 'http://www.youtube.com/embed/' + videoId;
+			const url = 'http://www.youtube.com/embed/' + videoId;
 
-	    const ytPlayer = $(`
+			const ytPlayer = $(`
 				<iframe
-	        id="player"
-	        type="text/html"
-	        width="640"
+					id="player"
+					type="text/html"
+					width="640"
 					height="390"
-	        src="${url}"
-	        frameborder="0"
-	      ></iframe>`);
+					src="${url}"
+					frameborder="0"
+				></iframe>`);
 
-	    $('#you-tube-container').html(ytPlayer);
+			$('#you-tube-container').html(ytPlayer);
 
 			resolve(videoId);
 		})
 		.catch(err => console.error(err));
 	});
-};
+	};
